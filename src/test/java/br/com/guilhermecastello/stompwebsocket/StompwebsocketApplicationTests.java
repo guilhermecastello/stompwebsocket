8491212dfb367932eab03017b98f62da54ac7a3f
Guilherme Castello <guilhermecastello@gmail.com>
2022-06-29 22:01:45 -0300

    Initial commit

---
+++ src/test/java/br/com/guilhermecastello/stompwebsocket/StompwebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guilhermecastello.stompwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StompwebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
